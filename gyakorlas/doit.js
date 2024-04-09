05f59d2992bd4dcf7e79285a73374377249a101d
Pistipisti94 <pistipisti57@gmail.com>
2024-04-09 13:59:22 +0200

    :c

--- gyakorlas/doit.js
+++ gyakorlas/doit.js
@@ -2,22 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const createButton = document.getElementById("create");
     var table = document.getElementById("azons");
     const formData = new FormData(document.getElementById("myForm"));
-    
-    console.log(formData);
-    
+
     createButton.addEventListener("click", async function () {
         const osszeg = document.getElementById("osszeg").value;
-        console.log(osszeg);
         let azons = document.getElementById("azons");
-        var valueazon = azons.value;
+        var valueazon = azons.value;  
+        console.log(osszeg);
         console.log(valueazon);
         let baseUrl = "http://localhost/Vizsga_Backend/index.php?befizetes";
         let options = {
             method: "POST",
             body: formData
         };
-        response = await fetch(baseUrl, options);
-        console.log(response);
+        console.log(formData);
+        response = await fetch(baseUrl, options);   
+        console.log(response);     
     })
     window.addEventListener("load", async (event) => {
         let baseUrl = "http://localhost/Vizsga_Backend/index.php?ugyfelek";
